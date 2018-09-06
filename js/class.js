9790180d57471598e7410262d85ec52b401b54d7
cooljohnny3 <cooljohnny3@gmail.com>
2018-09-06 14:07:47 -0700

    Added gitignore and some work.

--- js/class.js
+++ js/class.js
@@ -2,17 +2,18 @@
 // and a <div> element with the ID of "course-results"
 
 // query all CompSci courses
-var url = 'https://api.metalab.csun.edu/curriculum/api/2.0/classes/comp-110';
+var url = 'https://api.metalab.csun.edu/curriculum/api/2.0/classes/comp-100';
 $(document).ready(function() {
 	// perform a shorthand AJAX call to grab the information
 	$.get(url, function(data) {
 		// iterate over the returned courses
-		var courses = data.courses;
+    var courses = data.classes;
 		$(courses).each(function(index, course) {
-			// append each course to the content of the element
-			$('#course-results').append(
-        '<details><summary>' + course.subject + ' ' + course.catalog_number + ': ' + course.title + '</summary><p>' + course.description + '</p><a href="#">View Courses</a></details>'
-      );
+      if((meeting = course.meetings[0]) && (instructor = course.instructors[0])) {
+        $('#sections').append(
+          '<details id="section"><summary>' + course.catalog_number + instructor + course.title + '</summary><p>' + course.class_number + " " + meeting.location + " " + meeting.start_time + " " + meeting.end_time + " " + meeting.days + '</p><a href="#">View Courses</a></details>'
+        );
+      }
 		});
 	});
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  $('img[id^="delete_image"]').click(function() {
+    window.confirm("Are you sure you want to delete this class?");
+    console.log(this);
+  });
+
   $('#calendar').fullCalendar({
     defaultView: 'agendaWeek',
     header: false,
